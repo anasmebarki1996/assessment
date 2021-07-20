dc4aba73371c664c89e8e17b4ecf7ddcb4c39041
anas mebarki <anasmebarki1996@outlook.fr>
2021-07-20 15:09:25 +0100

    test done

--- js-numerals/src/index.ts
+++ js-numerals/src/index.ts
@@ -1,4 +1,4 @@
-const { arabicConversion1 } = require("./utils");
+const { arabicConversion1, arabicConversion2, arabicConversion3 } = require("./utils");
 
 
 console.log(42+"\t=== "+arabicConversion1.calculate(42));
@@ -9,4 +9,11 @@ console.log(17999+"\t=== "+arabicConversion1.calculate(17999));
 console.log(342251+"\t=== "+arabicConversion1.calculate(342251));
 console.log(1300420+"\t=== "+arabicConversion1.calculate(1300420));
 
-
+console.log("\n############### arabicConversion1 ###############")
+console.log(1999+"\t=== "+arabicConversion1.calculate(1999));
+console.log("\n############### arabicConversion2 ###############")
+console.log(1999+"\t=== "+arabicConversion2.calculate(1999));
+console.log(2999+"\t=== "+arabicConversion2.calculate(2999));
+console.log("\n############### arabicConversion3 ###############")
+console.log(2999+"\t=== "+arabicConversion3.calculate(2999));
+console.log("\n\n")
